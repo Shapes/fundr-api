b685441bf19790703fa88fcbf73e7c1eb2b0ef5f
Marko Balažic <marko.balazic@gmail.com>
2015-10-29 14:37:32 +0100

    Testna verzija deploya

--- cfservis/settings.py
+++ cfservis/settings.py
@@ -24,7 +24,7 @@ NEWSPIDER_MODULE = 'cfservis.spiders'
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY=3
+DOWNLOAD_DELAY=3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN=16
 #CONCURRENT_REQUESTS_PER_IP=16
@@ -87,4 +87,6 @@ ITEM_PIPELINES = {
 #HTTPCACHE_IGNORE_HTTP_CODES=[]
 #HTTPCACHE_STORAGE='scrapy.extensions.httpcache.FilesystemCacheStorage'
 
+
+# izklopi debug!
 #LOG_LEVEL = 'INFO'
